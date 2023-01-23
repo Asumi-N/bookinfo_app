6cac8ff0f051417c4b384f3346fb493c25cf896c
asumi <asumi@asuminoMacBook-Pro.local>
2023-01-23 15:24:56 +0900

    add/チェックボックス機能追加のためtagテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_034338) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_062332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
-    t.string "publish"
-    t.integer "isbn"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -34,6 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_034338) do
     t.string "author"
     t.integer "isbn"
     t.string "publisher"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
