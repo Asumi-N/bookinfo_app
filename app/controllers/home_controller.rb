3c78a9f11736aac391904dd0de43bd725a9e7e42
asumi <asumi@asuminoMacBook-Pro.local>
2023-01-27 11:50:00 +0900

    add/作品名もしくは著者名から検索機能を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def top
-    @search = Review.ransack(params[:q])
-    @posts = @search.result(distinct: true)
+    @q = Review.ransack(params[:q])
+    @results = @q.result(distinct: true)
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:title, thought1: [] )
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :set_q, only: [:index, :search]
   def new
     @review = Review.new
   end
@@ -8,7 +9,17 @@ class ReviewsController < ApplicationController
     @review.save
   end
 
+  def search
+    @results=@q.result
+  end
+
+  private
   def review_params
     params.require(:review).permit(:user_id, :title, :author, :publisher, :thought2, :isbn, thought1:[])
   end
+
+  def set_q
+    @q=Review.ransack(params[:q])
+  end
+
 end
