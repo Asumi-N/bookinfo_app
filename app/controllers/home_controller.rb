8d6e4888a80e6ce0b113402a347dff88d69402de
asumi <asumi@asuminoMacBook-Pro.local>
2023-01-27 16:46:58 +0900

    add/読後感から検索機能を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < ApplicationController
 
   private
     def review_params
-      params.require(:review).permit(:title, thought1: [] )
+      params.require(:review).permit(:title, :autohr, :thought2, thought1: [] )
     end
 end
