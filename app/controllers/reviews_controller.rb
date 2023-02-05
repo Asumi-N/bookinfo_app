aeffc34a42b4147555bdf77ba27ecb7fd1909928
asumi <asumi@asuminoMacBook-Pro.local>
2023-02-05 15:53:58 +0900

    update/レビュー新規投稿ページのエラーメッセージを表示

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to ("/reviews/#{@review.id}")
     else
-      render 'reviews/new'
+      render 'reviews/new', status: :unprocessable_entity
    end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,9 @@ class Review < ApplicationRecord
     has_many :tags, through: :review_tags
     has_many :review_tags
     accepts_nested_attributes_for :review_tags
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :publisher, presence: true
+    validates :thought1, presence: true
+    validates :thought2, presence: true
 end
