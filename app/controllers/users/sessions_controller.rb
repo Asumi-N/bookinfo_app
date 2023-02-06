3305d3ed96114b2e45fd5cce7e3b3c2a6f6b1c1c
asumi <asumi@asuminoMacBook-Pro.local>
2023-02-06 15:30:27 +0900

    update/ログイン・新規登録に失敗した際にエラーメッセージを表示させるためにdeviseの設定変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
-    def new_guest
-      user = User.guest
-      sign_in user  
-      redirect_to root_path
+  def new_guest
+    user = User.guest
+    sign_in user  
+    redirect_to root_path
+  end
+
+  def respond_to_on_destroy
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name), status: :see_other }
     end
   end
+  
+end
