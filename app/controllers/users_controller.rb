a9561ac54694d7e19bfab0f6f196c82752b64c21
asumi <asumi@asuminoMacBook-Pro.local>
2023-02-15 18:31:12 +0900

    add/ユーザー詳細画面にcssを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
-    @reviews=Review.where(user_id: @user.id)
+    @reviews=Review.where(user_id: @user.id).order(id: "DESC")
   end
 
   def create
